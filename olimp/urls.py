93835f28e9253c287853f94477d57f7d79d8fdad
KotlecikzZiemniaczkami <d.debicki03@gmail.com>
2024-03-26 14:12:51 +0100

    added view answering dynamically with even httpnotfound response

--- olimp/urls.py
+++ olimp/urls.py
@@ -6,6 +6,11 @@ from . import views
 # first argument is what will be in url, the second one is which function/class
 # should be executed when it is triggered
 urlpatterns = [
-    path('aphrodite', views.aphrodite)
+    path('aphrodite', views.aphrodite),
+    path('artemis', views.artemis),
+    path('ares', views.ares),
+    path('athena', views.athena),
+    path('antena', views.antena),
+    path('<name>', views.is_it_your_name)
 ]
 # now there is created sth called "url config"
--- olimp/views.py
+++ olimp/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # view which will have list of gods
 # btw if it's first view there is need to make new file "urls.py"
 def aphrodite(request):
     return HttpResponse("Aphrodite sends kiss to You")
+
+def artemis(request):
+    return HttpResponse("Artemis wishes you successful hunting")
+
+def ares(request):
+    return HttpResponse("Muuum, Hephaestus has stolen my knifeee!")
+
+def athena(request):
+    return HttpResponse("Ares is an idiot")
+
+def antena(request):
+    return HttpResponse("signal makes ziuuuuuu")
+
+# it is possible to work with arguments.
+def is_it_your_name(request, name):
+    if len(name) < 5:
+        text = 'Is ' + name + ' Your name? It is nice ;)'
+        return HttpResponse(text)
+    else:
+        return HttpResponseNotFound('404')
