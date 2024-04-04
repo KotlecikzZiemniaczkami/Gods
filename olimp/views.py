c4a7776ce29c76caf4ec86b21907bd544081a105
KotlecikzZiemniaczkami <d.debicki03@gmail.com>
2024-04-04 12:14:08 +0200

    new listing side

--- olimp/views.py
+++ olimp/views.py
@@ -1,38 +1,45 @@
+# render jus sends the html code as a response
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-# "in Django you can't just read template. You should convert it into string" this is for what you want render_to_string in your code
-from django.template.loader import render_to_string
 
 # answers given by gods
 god_answers = {
-    "zeus": "gimme coffee",
-    "hera": "gimme tea",
-    "athena": "wanna play chess?",
-    "antena": "you have fu*kn project to do!",
-    "artemis": "Do you want to relax by the fireplace? I have roasted wild boar"
+    "zeus": ["gimme coffee", "#FFFFE0"],
+    "hera": ["gimme tea",  	"#FFFF00"],
+    "athena": ["wanna play chess?", "#00CED1"],
+    "antena": ["you have fu*kn project to do!", "#A9A9A9"],
+    "artemis": ["Do you want to relax by the fireplace? I have roasted wild boar", "#228B22"]
 }
+
 
 def accessible_gods(request):
     httext = ''
     a_gods = list(god_answers.keys())
-    for i in range(len(a_gods)):
-        httext = httext + str(i+1) + '.' + f'<a href="{reverse("god-says", args=[a_gods[i]])}">' + a_gods[i].capitalize() + "</a><br>"
-    return HttpResponse(httext)
+    
+    return render(request, "olimp/choose_your_god.html", {"choose": a_gods})
 
 # mihological redirection
+
+
 def god_hierarchy(request, ask):
     # we will take their names by hierarchy
     greek_god_hierarchy = list(god_answers.keys())
-    
+
     # reverse dynamically builds a path to url named as given, and gives it given argument
     redirect_path = reverse("god-says", args=[greek_god_hierarchy[ask]])
     return HttpResponseRedirect(redirect_path)
-    
+
 # god has spoken ;]
+
+
 def god_says(request, ask):
     try:
-        return HttpResponse(render_to_string("olimp/gods_reaction.html"))
+        # it sends an html file (it needs to ad a request as an argument)
+        return render(request, "olimp/gods_reaction.html", {
+            "text": god_answers[ask][0],
+            "color": god_answers[ask][1],
+            "god_name": ask.capitalize()})
     # or not
     except:
         return HttpResponseNotFound("404\ngod not found")
